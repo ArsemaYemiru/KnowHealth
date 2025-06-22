033867a1415d13a7b6b9e325f28883979865659b
Arsema Yemiru <arsemayemiru610@gmail.com>
2025-06-22 21:19:53 +0300

    header and footer complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { Shell }  from "../components/Layout";
 import Hero from "../components/Hero";
 
 export default function Index() {
   return (
-    
+    <Shell>
       <Hero />
-      
+    </Shell>
   );
 }
